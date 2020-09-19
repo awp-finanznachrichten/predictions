e26cbd6612ab27468bdab23912a6b726964b7207
Simon Wolanin <sw@awp.ch>
2020-09-19 13:31:06 +0200

    Update 19.09.

--- predictions_run.R
+++ predictions_run.R
@@ -1,9 +1,11 @@
 #Load library and set wd
 source("config.R")
 
-#Season and Round
+#Season, Round and tips from players
 season <- "20/21"
 round <- "01"
+tips <- read_csv("BeatTheRobot/Beat the robot - Round 1 (Antworten) - Formularantworten 1.csv")
+
 
 #Get old predictions of robot
 predictions_robot_old <- read_csv("Output/predictions_SwissFootyBot.csv")
@@ -16,7 +18,6 @@ source("get_elodata.R", encoding = "UTF-8")
 
 #Marktwerte laden
 source("get_market_values.R", encoding = "UTF-8")
-
 
 #Scrape recently played matches
 source("get_new_data.R", encoding = "UTF-8")
@@ -32,4 +33,9 @@ source("predict_next_round.R")
 source("predict_season.R")
 
 ###Beat the robot
+
+#Predictions fans
+source("predictions_fans.R")
+
+#Evaluate current round
 source("beat_the_robot_run.R")
