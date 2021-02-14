68afc44d896711c19d94f4d7b96e281636ab9968
Simon Wolanin <sw@awp.ch>
2021-02-14 18:43:48 +0100

    Update 14.02.

--- beat_the_robot_run.R
+++ beat_the_robot_run.R
@@ -5,7 +5,6 @@ tips <- read_csv(tips_path)
 tips <- tips %>%
   distinct(`E-Mail-Adresse`, .keep_all=TRUE)
 
-
 #Evaluate score of the robot
 compare <- merge(last_results,predictions_robot_old)
 score_robot <- sum(compare$target == compare$Prediction)
@@ -40,10 +39,10 @@ for (i in 1:nrow(tips)) {
    tips$score[i] <- tips$score[i] + 1 
   }
 
-#  if (as.character(tips[i,8]) == as.character(last_results[5,2])) {
+  if (as.character(tips[i,8]) == as.character(last_results[5,2])) {
     
-#    tips$score[i] <- tips$score[i] + 1 
-#  }
+    tips$score[i] <- tips$score[i] + 1 
+  }
 
 
   if (tips$score[i] > score_robot) {
@@ -68,7 +67,6 @@ tips$fail <- nrow(last_results)-tips$score
 
 print(paste0(nrow(tips)," tips detected"))
 print(tips)
-
 
 #Save data of round
 save(tips,file=paste0("BeatTheRobot/tips_",round,".Rda"))
