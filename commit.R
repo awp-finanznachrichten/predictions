c9f10ce91ae2bd1d1ee4e27418c5dbaec558eb5e
awp-finanznachrichten <sw@awp.ch>
2021-03-17 13:03:38 +0100

    commit from Rstudio

--- commit.R
+++ commit.R
@@ -36,7 +36,10 @@ gitpush <- function(dir = getwd()){
 }
 
 #Make Commit
-git2r::config(user.name = "awp-finanznachrichten",user.email = "sw@awp.ch")
+token <- read.csv("C:/Users/simon/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
+
+#git2r::config(user.name = "awp-finanznachrichten",user.email = "sw@awp.ch")
+git2r::cred_token(token)
 gitadd()
 gitcommit()
 gitpush()
